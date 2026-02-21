b18793a2af63a4bb907c7ca55b9874834e0624c1
martinmejias12-ux <martin.mejias12@gmail.com>
2026-02-21 14:58:46 -0400

    Se realiza programa para los calculos de aislamiento, indice de polarizacin, indice de absorcion dielectrica, se corrigio el centrado del texto delarea del footer,

--- script/script.js
+++ script/script.js
@@ -85,16 +85,23 @@
         });
 
 
-
+        /**Para capturar datos de la pagina de HTML */
         const botonCalcular = document.getElementById('calcular');
         const tipoEquipo = document.getElementById('tipo-equipo');
 
         
-        
+        /**
+         * Lineas de programa para ejecutar cuando se presiona el botón de Calcular 
+         */
         botonCalcular.addEventListener('click', ()=>{
 
+            
+            /**
+             * Dependiendo del tipo de equipo que se seleccione se determinara el tipo de temperatura 
+             * de referencia a tomar en cuenta.
+             * 
+             */
             let temperaturaReferencia;
-
             switch (tipoEquipo.value) {
                 case "MOTOR":
 
@@ -116,11 +123,19 @@
             console.log(temperaturaReferencia);           
 
 
+            /**
+             * Lineas del programa para capturar el valor de temperatura de la carcasa del motor
+             * Adicionalmente se coloca una señal de alerta para indicar que ese campo no debe 
+             * estar vacío.
+             * También con el valor de la temperatura se determino el factor de corrección por temperatura
+             */
             const datoTemperatura = document.getElementById('temperatura');
             let valorTemperatura = parseFloat(datoTemperatura.value)
             console.log(valorTemperatura);
 
-            if (valorTemperatura.value === "" || Number.isNaN(valorTemperatura)) {
+            let factorCorreccionTemperatura = 0;
+
+            if (Number.isNaN(valorTemperatura)) {
 
                 alert('El campo de "Temperatura de la carcasa del motor" no debe estar vacío');
                 
@@ -128,16 +143,100 @@
                 
             }else{
 
-                let factorCorreccionTemperatura = 0.5 ** ((temperaturaReferencia-valorTemperatura)/10)
+                factorCorreccionTemperatura = 0.5 ** ((temperaturaReferencia-valorTemperatura)/10)
 
-                factorCorreccionTemperatura = Math.round(factorCorreccionTemperatura*1000)/1000
+                factorCorreccionTemperatura = Math.round(factorCorreccionTemperatura*100)/100
 
                 console.log(factorCorreccionTemperatura);
 
 
             }
 
+
+            const datoMedicionAislamiento30seg= document.getElementById('valor30seg');
+            let valorMedicionAislamiento30seg = parseFloat(datoMedicionAislamiento30seg.value);
+
+            const datoMedicionAislamiento1min= document.getElementById('valor1min');
+            let valorMedicionAislamiento1min = parseFloat(datoMedicionAislamiento1min.value);
+
+            const datoMedicionAislamiento10min= document.getElementById('valor10min');
+            let valorMedicionAislamiento10min = parseFloat(datoMedicionAislamiento10min.value);
+
             
+            console.log(valorMedicionAislamiento30seg);
+            console.log(valorMedicionAislamiento1min);
+            console.log(valorMedicionAislamiento10min);
+
+            
+        
+            /**
+             * Para el calculo del valor de aislamiento
+             */
+
+            let valorCorregidoLectura1min = 0;
+            if (Number.isNaN(valorMedicionAislamiento1min)) {
+
+                alert('El campo de la lectura medida al 1 min esta vacío');
+                
+                
+                
+            }else{
+
+                valorCorregidoLectura1min = factorCorreccionTemperatura * valorMedicionAislamiento1min;
+
+                valorCorregidoLectura1min = Math.round(valorCorregidoLectura1min*100)/100
+
+                console.log(valorCorregidoLectura1min);
+
+
+            }
+
+
+            /**
+             * Para el calculo del indice de absorción dieléctrica (DA)
+             */
+
+            let valorIndiceAbsorcionDielectrica = 0;
+            if (Number.isNaN(valorMedicionAislamiento30seg)) {
+
+                alert('El campo de la lectura medida a los 30 seg esta vacío');
+                
+                
+                
+            }else{
+
+                valorIndiceAbsorcionDielectrica =  valorMedicionAislamiento1min/valorMedicionAislamiento30seg;
+
+                valorIndiceAbsorcionDielectrica = Math.round(valorIndiceAbsorcionDielectrica*100)/100
+
+                console.log(valorIndiceAbsorcionDielectrica);
+
+
+            }
+
+
+            /**
+             * Para el calculo del indice de polarización (IP)
+             */
+
+            let valorIndicePolarizacion = 0;
+            if (Number.isNaN(valorMedicionAislamiento10min)) {
+
+                alert('El campo de la lectura medida a los 30 seg esta vacío');
+                
+                
+                
+            }else{
+
+                valorIndicePolarizacion =  valorMedicionAislamiento10min/valorMedicionAislamiento1min;
+
+                valorIndicePolarizacion = Math.round(valorIndicePolarizacion*100)/100
+
+                console.log(valorIndicePolarizacion);
+
+
+            }
+
 
 
 
@@ -147,9 +246,10 @@
 
         
 
-
-        const borrarDatos =document.getElementById('reset');
-    
+        /**
+         * Programa para borrar los datos del localStorage y de la pantalla
+         */
+        const borrarDatos =document.getElementById('reset');    
         borrarDatos.addEventListener('click', ()=>{
 
             listaCampos.forEach(elemento =>{
